9cc84e20d87c42680f6b28078959a1f67e2919d0
Mihkel <=>
2023-11-20 19:46:11 +0200

    routines

---
+++ dumps/routines/maintenance.sql
@@ -0,0 +1,119 @@
+-- MariaDB dump 10.19  Distrib 10.6.12-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: maintenance
+-- ------------------------------------------------------
+-- Server version	10.2.25-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `maintenance`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `maintenance` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_estonian_ci */;
+
+USE `maintenance`;
+
+--
+-- Dumping routines for database 'maintenance'
+--
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `populate_sugu` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`michelek`@`127.0.0.1` PROCEDURE `populate_sugu`()
+BEGIN
+
+ UPDATE repis.kirjed k0
+-- SELECT * FROM  repis.kirjed k0
+right join
+(
+  SELECT persoon, GROUP_CONCAT(distinct Sugu) sugu
+  FROM repis.kirjed
+  WHERE persoon > 0
+  GROUP BY persoon
+  HAVING LENGTH(sugu) > 1
+) sood 
+ON sood.persoon = k0.persoon
+SET k0.Sugu = RIGHT(sood.sugu,1)
+WHERE sood.sugu LIKE ',%'
+;
+
+UPDATE repis.kirjed k0
+-- SELECT * FROM  repis.kirjed k0
+right join
+(
+  SELECT persoon, GROUP_CONCAT(distinct Sugu) sugu
+  FROM repis.kirjed
+  WHERE persoon > 0
+  GROUP BY persoon
+  HAVING LENGTH(sugu) > 1
+) sood 
+ON sood.persoon = k0.persoon
+ SET k0.Sugu = LEFT(sood.sugu,1)
+WHERE sood.sugu LIKE '%,'
+;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `update_wwiiref` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`michelek`@`127.0.0.1` PROCEDURE `update_wwiiref`(
+	IN `in_persoon` CHAR(10)
+)
+BEGIN
+
+  SELECT persoon 
+  INTO @real_persoon 
+  FROM repis.kirjed 
+  WHERE kirjekood = in_persoon;
+
+  SELECT MAX(pub_wwiiref)
+  INTO @is_ref
+  FROM repis.kirjed
+  WHERE persoon = @real_persoon
+  AND kirjekood != @real_persoon;
+  
+  UPDATE repis.kirjed
+  SET pub_wwiiref = @is_ref
+  WHERE kirjekood = in_persoon;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
