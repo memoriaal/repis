af014582f71190994ba93e1880050a23e5c7095b
Mihkel <=>
2023-11-20 18:02:08 +0200

    drop

--- scripts/drop_tunnel.sh
+++ scripts/drop_tunnel.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Load environment variables
-source ../.env || source .env
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$script_dir/../.env"
 
 # Check if SSH tunnel is established
 if nc -z localhost $SSH_LOCAL_PORT; then
-    # Terminate SSH tunnel to mysql proxy
-    echo "Closing SSH tunnel..."
-    ssh -O exit -T -M -L $SSH_LOCAL_PORT:
-    echo "SSH tunnel closed."
+    # find processes with the same port number and kill them
+    echo "Killing processes with port $SSH_LOCAL_PORT..."
+    ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep | awk '{print $2}' | grep -v "PID" | xargs kill
+    echo "ssh processes killed."
 else
     echo "No tunnel to close."
 fi
