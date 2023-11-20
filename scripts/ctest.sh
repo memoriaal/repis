2f92b63cb186b03e88c71c83f5fde304a8e7d349
Mihkel <=>
2023-11-20 18:21:34 +0200

    kill tunnels

--- scripts/ctest.sh
+++ scripts/ctest.sh
@@ -1,8 +1,8 @@
 if nc -z localhost $SSH_LOCAL_PORT; then
     now=`mysql --host=127.0.0.1 --port=3306 -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" pub<<EOFMYSQL
-    select now();
-    EOFMYSQL
-    `
+select now();
+EOFMYSQL
+`
     echo "Current time at server: ${now}"
 else
     echo "No tunnel. Or is it?"
--- scripts/drop_tunnel.sh
+++ scripts/drop_tunnel.sh
@@ -4,11 +4,12 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$script_dir/../.env"
 
-# test with 'ps aux' if there are any processes with the same port number
-if ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep; then
-    echo "Killing processes with port $SSH_LOCAL_PORT..."
-    ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep | awk '{print $2}' | grep -v "PID" | xargs kill
-    echo "ssh processes killed."
-else
-    echo "SSH tunnel is not established."
-fi
+signature="ssh -f -N -T -M -L ${SSH_LOCAL_PORT}"
+# collect processes matching the signature
+processes=`ps aux | grep "${signature}" | grep -v grep | awk '{print $2}' | grep -v "PID"`
+
+# Kill all processes matching the signature
+for process in $processes; do
+    echo "Killing process ${process}..."
+    kill $process
+done
