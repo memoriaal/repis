8a9b5850690868c7a294a9b7f6699b7018929f90
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-20 17:51:30 +0200

    Update ctest.sh

--- scripts/ctest.sh
+++ scripts/ctest.sh
@@ -1,5 +1,10 @@
-now=`mysql --host=127.0.0.1 --port=3306 -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" pub<<EOFMYSQL
-select now();
-EOFMYSQL
-`
-echo "Current time at server: ${now}"
+if nc -z localhost $SSH_LOCAL_PORT; then
+    now=`mysql --host=127.0.0.1 --port=3306 -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" pub<<EOFMYSQL
+    select now();
+    EOFMYSQL
+    `
+    echo "Current time at server: ${now}"
+else
+    echo "No tunnel. Or is it?"
+    ps aux | grep "ssh -f -N -T -M -L 3306" | grep -v grep
+fi
