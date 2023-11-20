81718e6be21d4ce53218c35f0e0005ac7ec9fe80
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-20 19:33:44 +0200

    Update dump_routines.sh

--- scripts/dump_routines.sh
+++ scripts/dump_routines.sh
@@ -13,10 +13,9 @@ source "$script_dir/../.env"
 for db in $MYSQL_DATABASES; do
   echo "Dumping routines for database ${db}..."
   if nc -z localhost $SSH_LOCAL_PORT; then
-        mysqldump -h 127.0.0.1 --port=$SSH_LOCAL_PORT -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --databases ${db} --routines --no-create-info --no-data --skip-triggers > dumps/routines/${db}.sql
+    mysqldump -h 127.0.0.1 --port=$SSH_LOCAL_PORT -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --databases ${db} --routines --no-create-info --no-data --skip-triggers > dumps/routines/${db}.sql
   else
       echo "No tunnel. Or is it?"
       ps aux | grep "ssh -f -N -T -M -L 3306" | grep -v grep
-      exit 1
   fi
 done
