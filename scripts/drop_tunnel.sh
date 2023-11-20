893f5cbeb5d01922f3422c04e698a3521ed3809b
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-20 18:07:33 +0200

    Update drop_tunnel.sh

--- scripts/drop_tunnel.sh
+++ scripts/drop_tunnel.sh
@@ -4,7 +4,11 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$script_dir/../.env"
 
-# find processes with the same port number and kill them
-echo "Killing processes with port $SSH_LOCAL_PORT..."
-ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep | awk '{print $2}' | grep -v "PID" | xargs kill
-echo "ssh processes killed."
+# test with 'ps aux' if there are any processes with the same port number
+if ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep; then
+    echo "Killing processes with port $SSH_LOCAL_PORT..."
+    ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep | awk '{print $2}' | grep -v "PID" | xargs kill
+    echo "ssh processes killed."
+else
+    echo "SSH tunnel is not established."
+fi
