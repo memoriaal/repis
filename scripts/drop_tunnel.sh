a9bf46352a55d39f986fb13f91bf54884b033310
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-20 18:03:51 +0200

    Update drop_tunnel.sh

--- scripts/drop_tunnel.sh
+++ scripts/drop_tunnel.sh
@@ -4,12 +4,7 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$script_dir/../.env"
 
-# Check if SSH tunnel is established
-if nc -z localhost $SSH_LOCAL_PORT; then
-    # find processes with the same port number and kill them
-    echo "Killing processes with port $SSH_LOCAL_PORT..."
-    ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep | awk '{print $2}' | grep -v "PID" | xargs kill
-    echo "ssh processes killed."
-else
-    echo "No tunnel to close."
-fi
+# find processes with the same port number and kill them
+echo "Killing processes with port $SSH_LOCAL_PORT..."
+ps aux | grep "ssh -f -N -T -M -L ${SSH_LOCAL_PORT}" | grep -v grep | awk '{print $2}' | grep -v "PID" | xargs kill
+echo "ssh processes killed."
