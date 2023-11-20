f29e96f0f4faff9052b97dc64c9c61ee6813c999
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-20 20:05:21 +0200

    Create repis2git.sh

---
+++ repis2git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./scripts/establish_tunnel.sh
+
+./scripts/dump_schemas.sh
+./scripts/dump_routines.sh
+
+./scripts/close_tunnel.sh
+
+# commit changes
+git add .
+git commit -m "Update schemas, functions and procedures"
+git push origin master
