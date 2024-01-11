736b41dc7a99ac97d7ce57a59c118288bb7da6cb
Mihkel <=>
2024-01-12 01:42:15 +0200

    Update schemas, functions and procedures

--- dumps/events/import.sql
+++ dumps/events/import.sql
@@ -156,3 +156,62 @@ END */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`michelek`@`127.0.0.1`*/ /*!50003 TRIGGER `el_kart_BU` BEFORE UPDATE ON `el_kart` FOR EACH ROW BEGIN
+	if true then
+	   IF NEW.persoon = '0' THEN
+	   	SELECT LPAD(MAX(persoon)+1, 10, '0') FROM repis.kirjed INTO @new_persoon;
+	   	INSERT INTO repis.kirjed (persoon, kirjekood, allikas)
+	   	VALUES (@new_persoon, @new_persoon, 'persoon');
+	   	SET NEW.persoon = @new_persoon;
+	   END IF;
+
+	   IF length(NEW.persoon) = 10 THEN
+	      UPDATE IGNORE repis.kirjed
+	      SET persoon = NEW.persoon
+	      WHERE kirjekood = NEW.kirjekood;
+	   END IF;
+	
+	   IF NEW.persoon IS NULL THEN
+	      UPDATE IGNORE repis.kirjed
+	      SET persoon = NULL
+	      WHERE kirjekood = NEW.kirjekood;
+	   END IF;
+	END if;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`michelek`@`127.0.0.1`*/ /*!50003 TRIGGER `el_kart_AU` AFTER UPDATE ON `el_kart` FOR EACH ROW BEGIN
+	if true then
+		if NEW.persoon <> IFNULL(OLD.persoon, 'NULL')
+		then
+	  		CALL import.import_ELK(NEW.persoon, NEW.kirjekood);
+	  		CALL repis.proc_NK_refresh(NEW.persoon, NEW.kirjekood);
+	  	END if;
+  	END if;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
