8de98c72ff3d9e6681a80dbbe9e60dd806526c8f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-01-17 10:54:41 +0200

    reverse logic on check, if tunnel is already established

--- scripts/establish_tunnel.sh
+++ scripts/establish_tunnel.sh
@@ -4,19 +4,14 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$script_dir/../.env"
 
-# Check if SSH tunnel is already established
-if nc -z localhost $SSH_LOCAL_PORT; then
-    echo "SSH tunnel is already established."
-else
+# Check if SSH tunnel is not established
+if ! nc -z localhost $SSH_LOCAL_PORT; then
     # Establish SSH tunnel to mysql proxy
     # (control file and SSH_HOST in ~/.ssh/config)
-    echo "Establishing SSH tunnel..."
     ssh -f -N -T -M -L $SSH_LOCAL_PORT:127.0.0.1:$SSH_REMOTE_PORT $SSH_HOST
 
     # Check if SSH tunnel was successfully established
-    if nc -z localhost $SSH_LOCAL_PORT; then
-        echo "SSH tunnel successfully established."
-    else
+    if ! nc -z localhost $SSH_LOCAL_PORT; then
         echo "Failed to establish SSH tunnel."
         exit 1
     fi
