18ddb5c60f86cf524da7a6b2908afba538919188
Mihkel <=>
2024-02-03 00:43:01 +0200

    Update schemas, functions and procedures

--- dumps/schemas/aruanded.sql
+++ dumps/schemas/aruanded.sql
@@ -595,16 +595,3 @@ USE `aruanded`;
 
 --
 -- Final view structure for view `segased_sood`
---
-
-
---
--- Final view structure for view `topelt_kivikirjed`
---
-
-
---
--- Final view structure for view `uued_kivikirjed`
---
-
-
